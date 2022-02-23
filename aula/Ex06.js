f2a702754c19a11a0944f4178904861548c24fb4
Rulyan Eduardo dos <54826263+Rulyan@users.noreply.github.com>
2022-02-23 18:54:43 -0300

    Exercícios adicionados

---
+++ aula/Ex06.js
@@ -0,0 +1,18 @@
+/* Faça um script que determine dentre dois números qual é o maior número */
+
+var n1, n2
+
+n1=9
+n2=3
+
+if(n1>=n2){
+    alert(n1+" é maior ou igual que "+n2)
+}
+else if{
+    alert(n2+" é maior que "+n1)
+}
+    console.log(n2+" é maior que "+n1)
+    
+else{
+        alert(n1 + " igual a " + n2)
+    }
---
+++ aula/Ex07.js
@@ -0,0 +1,15 @@
+/* Crie um script que leia a idade do usuário e se ele passou no teste prático para obter a CNH.
+ Caso tenha passado nos dois requisitos estará APTO caso contrário INAPTO, e dizer em qual quesito reprovou. */
+ 
+ var idad, n1=18
+
+ if(idade>=18){
+     tp=prompt("Passou no teste prático?(s/n)")
+     if(tp=="s")
+     {
+        alert("Parabéns! Você pode tirar a CNH")
+     }
+ }
+ else{
+     
+ }
